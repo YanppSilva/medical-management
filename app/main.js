14a77ebccff6db286e474899cff88e00da955fa3
Yan Pablo <93152439+YanppSilva@users.noreply.github.com>
2023-06-28 17:05:07 -0300

    Add files via upload
    Construção da tela de Login e inicio da validação do formulário da tela de Cadastro.

---
+++ app/main.js
@@ -0,0 +1,54 @@
+const form = document.querySelector("#form");
+const emailInput = document.querySelector("#email");
+const numConselho = document.querySelector("#numero_conselho");
+const senhaInput = document.querySelector("#senha");
+const confirmaSenha = document.querySelector("#confirmar_senha");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if(emailInput.value === "") {
+    alert("Por favor, preencha seu email");
+    return;
+  }
+
+  if(emailInput.value === "" || !emailValido(emailInput.value)) {
+    alert("Por favor, preencha um email válido. Ex.: usuário@host.com");
+    return;
+  }
+
+  if(senhaInput.value === "") {
+    alert("Por favor, preencha sua senha");
+    return;
+  }
+
+  if(senhaInput.value === "" || !validaSenha(senhaInput.value)) {
+    alert("A senha deve conter no mínimo 6 e 15 caracteres, contendo uma letra e um número");
+    return;
+  }
+
+  form.submit();
+});
+
+function emailValido(email) {
+  const emailRegex = new RegExp(
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+  );
+
+  if(emailRegex.test(email)) {
+    return true;
+  } return false;
+}
+
+function validaSenha(password) {
+  const senhaRegex = new RegExp(
+    /^(?=.*[A-Za-z])(?=.*\d).{6,15}$/
+  );
+
+  if(senhaRegex.test(password)) {
+    return true;
+  } return false;
+}
+
+
+
